0550c4c07fe1ff8b274de0e298c1a422c56ca5c9
Gavin <waltersg298@gmail.com>
2023-09-20 17:40:07 -0600

    pushing all the recently completed challenges that were done about a month ago

--- medium/array_of_products.py
+++ medium/array_of_products.py
@@ -1,3 +1,11 @@
+
+'''
+Write a function that takes in a non-empty array of integers and returns an array of the same size, where each element in the
+output array is equal to the product of every other number in the input array. 
+
+In other words, output[i] is equal to the product every number in the input array other than input[i]. 
+'''
+
 def arrayOfProducts(array):
     output = []
 
--- medium/best_seat.py
+++ medium/best_seat.py
@@ -1,3 +1,16 @@
+
+'''
+You walk into a theatre you're about to see the show in. The usher within the theatre walks you to your row and mentions, 
+and mentions your're allowed to sit anywhere in a given row. Naturally, you'd like to sit in a sit that gives you the most
+space. You would also prefer that this space be evenly distributed on either side of you (e.g. if there are three empty
+seats in a row, you would prefer to sit in the middle of those three seats). 
+
+Given the theatre row represented as an integer array, return the seat index of where you should sit. Ones represent
+occupied seats and zeroes represent empty seats. You may assume that someone is always sitting in the first and last seat 
+of the row. Whenever there are two equally good seats, you should sit in the seat with the lower index. If there is no seat 
+to sit in, return -1. The given array will always have a length of at least one and contain only ones and zeroes.
+'''
+
 def bestSeat(seats):
     best_seat = -1 
     max_space = 0
@@ -16,3 +29,4 @@ def bestSeat(seats):
         left = right 
     
     return best_seat
+
