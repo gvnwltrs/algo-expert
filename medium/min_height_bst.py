7de96e80417f2280441f2a0e935f9071cf189098
Gavin <waltersg298@gmail.com>
2023-12-11 10:42:21 -0700

    updated algo -- still broken

--- medium/min_height_bst.py
+++ medium/min_height_bst.py
@@ -1,9 +1,29 @@
 
 # The goal here should be to have as much of a 
 # balanced tree as possible. 
+#
 
 def minHeightBst(array):
-    pass
+    return constructMinHeights(array, None, 0, len(array) - 1)
+
+def constructMinHeighBst(array, bst, startIdx, endIdx):
+    if endIdx < startIdx:
+        return
+    midIdx = (startIdx + endIdx) // 2
+    if bst is None:
+    newBstNode = BST(array[midIdx])
+        bst = newBstNode
+    else:
+        if array[midIdx] < bst.value:
+            bst.left = newBstNode 
+            bst = bst.left
+        elif array[midIdx] > bst.value:
+            bst.right = array[midIdx]
+            bst = bst.right
+
+    constructMinHeightBst(array, bst, startIdx, midIdx - 1)
+    constructMinHeightBst(array, bst, midIdx + 1, endIdx)
+    return bst
 
 
 class BST:
