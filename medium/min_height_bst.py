c1a5323da1c0e02ff3a5d5f14c9dee28f8802c32
Gavin <waltersg298@gmail.com>
2023-12-11 14:22:46 -0700

    fixed issue -- be mindful of new lazyvim config!!!!!!

--- medium/min_height_bst.py
+++ medium/min_height_bst.py
@@ -1,28 +1,28 @@
 
 # The goal here should be to have as much of a 
 # balanced tree as possible. 
-#
 
+#
 def minHeightBst(array):
     return constructMinHeights(array, None, 0, len(array) - 1)
 
 def constructMinHeighBst(array, bst, startIdx, endIdx):
-    if endIdx < startIdx:
+    if endIdx < startIdx: # if endidx has crossoved over startidx break
         return
-    midIdx = (startIdx + endIdx) // 2
+    midIdx = (startIdx + endIdx) // 2 # find our middle
+    newBstNode = BST(array[midIdx]) # setup new node with new value
     if bst is None:
-    newBstNode = BST(array[midIdx])
-        bst = newBstNode
+        bst = newBstNode # if first node create new and start building 
     else:
-        if array[midIdx] < bst.value:
-            bst.left = newBstNode 
+        if array[midIdx] < bst.value: # if new node is less than current node set current node left to new newnode and jump to that new node
+            bst.left = newBstNode   
             bst = bst.left
         elif array[midIdx] > bst.value:
-            bst.right = array[midIdx]
+            bst.right = newBstNode
             bst = bst.right
 
-    constructMinHeightBst(array, bst, startIdx, midIdx - 1)
-    constructMinHeightBst(array, bst, midIdx + 1, endIdx)
+    constructMinHeightBst(array, bst, startIdx, midIdx - 1) # recurse left to current middle
+    constructMinHeightBst(array, bst, midIdx + 1, endIdx) # recurse right to current middel
     return bst
 
 
