abf40b155355fb1d6283a748992dc59cacff0cce
Gavin <waltersg298@gmail.com>
2023-12-12 17:31:14 -0700

    solved in o(n)

--- medium/find_kth_largest_value_in_bst.py
+++ medium/find_kth_largest_value_in_bst.py
@@ -1,5 +1,12 @@
-# This is an input class. Do not edit.
+
+'''
+
+Write a function that takes in a BST and a positive integer 'k' and returns the kth largest contained in BST. 
+'''
+
 class BST:
+
+    # This is an input class. Do not edit.
     def __init__(self, value, left=None, right=None):
         self.value = value
         self.left = left
@@ -8,5 +15,22 @@ class BST:
 
 def findKthLargestValueInBst(tree, k):
     # Write your code here.
+    print(self.value)
+    storage = []
+    flat_box = flatten_tree(tree, storage) 
+    # sort flat_box ascending
+    flat_box.sort()
+    return flat_box[-k]
 
-    return -1
+def flatten_tree(node, container):
+    container.append(node.value)
+
+    if node.left is not None:
+        flatten_tree(node.left, container)
+    if node.right is not None:
+        flatten_tree(node.right, container)
+
+    return container 
+
+
+
