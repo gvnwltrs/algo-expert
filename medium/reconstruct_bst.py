81eadc8f88260f594e30f3ae330ff0f2734a10e3
Gavin <waltersg298@gmail.com>
2023-12-14 15:09:06 -0700

    updated solution -- WIP

--- medium/reconstruct_bst.py
+++ medium/reconstruct_bst.py
@@ -1,4 +1,14 @@
 
+# Problem: the pre-order traversal of a Binary Tree is a traversal technique
+# that starts at teh tree's root node and vists nodes in the following order:
+#   1. current node
+#   2. left subtree
+#   3. right subtree
+# Given a non-empty array of integers representing the pre-order preOrderTraversalValues
+# of a Binary Search Tree (BST), write a fucntion that creates the relevant BST and 
+# returns its root node. 
+
+
 # This is an input class. Do not edit.
 class BST:
     def __init__(self, value, left=None, right=None):
@@ -8,5 +18,19 @@ class BST:
 
 
 def reconstructBst(preOrderTraversalValues):
-    # Write your code here.
-    return None
+    return createBst(BST(preOrderTraversalValues.pop(0)), preOrderTraversalValues)
+
+def createBst(root, values):
+    if values is None or values[1] is None:
+        return
+    next_node = BST(values.pop(0))
+
+    if next_node.value < root.value:
+        root.left = next_node.value
+        createBst(next_node, values)
+
+    if root.value > root.value:
+        root.right = next_node.value
+        createBst(next_node, values)
+
+    return root
