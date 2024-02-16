a8a5372f100230c5fd1c3c78b21ce36ed571c37b
Gavin <waltersg298@gmail.com>
2024-02-16 09:04:32 -0700

    breaking down transpose matrix solution

--- beginner/transpose_matrix.py
+++ beginner/transpose_matrix.py
@@ -9,3 +9,12 @@ def transposeMatrix(matrix):
             transpose_matrix[i][j] = matrix[j][i]
 
     return transpose_matrix
+
+# time complexity: O(n * m) where n is the number of rows and m is the number of columns | space complexity: O(n * m) where n is the number of rows and m is the number of columns
+'''
+vertical matrix = [[1], [2], [3]]
+horizontal matrix = [[1, 2, 3]]
+
+flipped vertical matrix = [[1, 2, 3]]
+flipped horizontal matrix = [[1], [2], [3]]
+'''
